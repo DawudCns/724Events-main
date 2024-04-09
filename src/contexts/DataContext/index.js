040aba964aeb1df4f6a1eb6e3656d2e8fd642e30
Dawud Cns <davidhc@hotmail.be>
2024-04-09 02:09:18 +0200

    modification

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -26,17 +26,25 @@ export const DataProvider = ({ children }) => {
       setError(err);
     }
   }, []);
+
+  const events = data?.events;
+  const sortedEvents = events?.sort((evtA, evtB) =>
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+  );
+  const last = sortedEvents?.[0];
+
   useEffect(() => {
     if (data) return;
     getData();
   });
-  
+
   return (
     <DataContext.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
       value={{
         data,
         error,
+        last,
       }}
     >
       {children}
@@ -46,7 +54,7 @@ export const DataProvider = ({ children }) => {
 
 DataProvider.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
 export const useData = () => useContext(DataContext);
 
